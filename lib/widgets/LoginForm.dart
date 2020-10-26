d94ed2534ff1c19aeca4799be66912a66b9f5fe2
michaelshabib <63905853+michaelshabib@users.noreply.github.com>
2020-10-26 21:22:23 +0200

    Login form validation

--- lib/widgets/LoginForm.dart
+++ lib/widgets/LoginForm.dart
@@ -11,9 +11,12 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
-  String signInEmail = "";
-  String signInPassword = "";
-  bool isTryingToSignIn = false;
+  String _signInEmail = "";
+  String _signInPassword = "";
+  bool   _isTryingToLogIn = false;
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     
@@ -25,63 +28,80 @@ class _LoginFormState extends State<LoginForm> {
     );
     
 
-    return Form(
-      child: Column(
-        children: [
-          //Email Field
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 3),
-            child: TextFormField(
-              decoration: textFieldDecoration.copyWith(hintText: "Email"),
-              style: TextStyle(color: Colors.black, fontSize: 20),
-              onChanged: (email) {signInEmail = email;},
-            ),
-          ),
-          //Password Field
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 3),
-            child: TextFormField(
-              obscureText: true, //password type
-              decoration: textFieldDecoration.copyWith(hintText: "Password"),
-              style: TextStyle(color: Colors.black, fontSize: 20),
-              onChanged: (password) {signInPassword = password;},
-            ),
-          ),
-          //Login Button
-          FlatButton(
-            padding: EdgeInsets.zero,
-            child: Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height/15,
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-                borderRadius: BorderRadius.circular(10)
-              ),
-              child: Center(
-                child: Text( "Login",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 24,
-                  ),
+    return Stack(
+      children: [
+        Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              //Email Field
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 3),
+                child: TextFormField(
+                  decoration: textFieldDecoration.copyWith(hintText: "Email"),
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                  onChanged: (email) {_signInEmail = email;},
+                  validator: (email) {return email.isEmpty? "Please fill your Email" : null;},
                 ),
               ),
-            ),
-            onPressed: () {
-              _signIn();
-            },
-          )
-        ],
-      ),
+              //Password Field
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 3),
+                child: TextFormField(
+                  obscureText: true, //password type
+                  decoration: textFieldDecoration.copyWith(hintText: "Password"),
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                  onChanged: (password) {_signInPassword = password;},
+                  validator: (email) {return email.isEmpty? "Please fill your Password" : null;},
+                ),
+              ),
+              //Login Button
+              FlatButton(
+                padding: EdgeInsets.zero,
+                child: Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height/15,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Center(
+                    child: Text( "Login",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+                onPressed: () {
+                  _logIn();
+                },
+              )
+            ],
+          ),
+        ),
+
+        if(_isTryingToLogIn)
+           Center(child: CircularProgressIndicator()),
+      ]
     );
   }
 
-  void _signIn() async {
-    
+  void _logIn() async {
+
+    if(!_formKey.currentState.validate())
+      return;
+
     setState(() {
-      isTryingToSignIn = true;
+      _isTryingToLogIn = true;
     });
 
-    bool status = await Provider.of<Auth>(context, listen: false).logIn(signInEmail, signInPassword, "iphone11");
+    bool status = await Provider.of<Auth>(context, listen: false).logIn(_signInEmail, _signInPassword, "iphone11");
+
+    setState(() {
+      _isTryingToLogIn = false;
+    });
 
     if (status) {
       //go to user's Home
@@ -91,8 +111,6 @@ class _LoginFormState extends State<LoginForm> {
       );
     }
     
-    setState(() {
-      isTryingToSignIn = false;
-    });
+
   }
 }
