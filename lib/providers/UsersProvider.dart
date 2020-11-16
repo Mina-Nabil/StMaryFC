35fb2e304550f620fe1e673aea5ebc29111adc73
Mina Nabil <mina9492@hotmail.com>
2020-11-16 22:10:04 +0200

    Adding user from json

--- lib/providers/UsersProvider.dart
+++ lib/providers/UsersProvider.dart
@@ -7,60 +7,41 @@ import "package:http/http.dart" as http;
 import '../global.dart';
 
 class UsersProvider with ChangeNotifier {
-
   UsersProvider(this._token);
-  
+
   String _token;
   String _searchApiUrl = Server.address + "api/search/name";
   String _attendanceApiUrl = Server.address + "api/take/bulk/attendance";
 
   // search results
-  List<User> _users= [];
+  List<User> _users = [];
 
-  Future<void> search (String searchString) async {
-
+  Future<void> search(String searchString) async {
     final response = await http.post(
       _searchApiUrl,
-      headers: {
-        'Authorization': "Bearer $_token",
-        "Accept": "application/json"
-      },
+      headers: {'Authorization': "Bearer $_token", "Accept": "application/json"},
       body: {
         'name': searchString,
       },
-
     );
 
     dynamic body = jsonDecode(response.body);
-    
-    _users.clear();
-    if(body["message"] != null)
-    {
-      for (var user in body["message"]) {
-        int id           = user["id"];
-        String name      = user["USER_NAME"];
-        String groupName = user["GRUP_NAME"];
-        String imageLink = "";
-        if(user["USIM_URL"] != null)
-          imageLink = "https://stmaryfa.msquare.app/storage/" + user["USIM_URL"];
-        _users.add(User(id, name, groupName, imageLink));
-      }
-    } else {
 
-    }
+    _users.clear();
+    if (body["message"] != null) {
+      for (var user in body["message"]) {
+        _users.add(User.fromJson(user));
+      }
+    } else {}
 
     //print(_users);
     notifyListeners();
   }
 
-  Future<bool> takeAttendance (Set<int> ids, String date) async {
-
+  Future<bool> takeAttendance(Set<int> ids, String date) async {
     final response = await http.post(
       _attendanceApiUrl,
-      headers: {
-        'Authorization': "Bearer $_token",
-        "Accept": "application/json"
-      },
+      headers: {'Authorization': "Bearer $_token", "Accept": "application/json"},
       body: {
         'userIDs': ids.toString(),
         'date': date,
@@ -68,14 +49,14 @@ class UsersProvider with ChangeNotifier {
     );
     dynamic body = jsonDecode(response.body);
 
-    if(body["status"] != null && body["status"] == true){
+    if (body["status"] != null && body["status"] == true) {
       print("Attendance taken correct!");
       return true;
-    }
-    else
+    } else
       return false;
   }
-  
 
-  List<User> get users {return _users;}
+  List<User> get users {
+    return _users;
+  }
 }
