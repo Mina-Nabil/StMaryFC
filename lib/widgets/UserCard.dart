7d349ec8f4a7977d94caf13bdc7942a081dd7348
Mina Nabil <mina9492@hotmail.com>
2020-11-17 11:55:56 +0200

    Adjust select/isAttended colors

--- lib/widgets/UserCard.dart
+++ lib/widgets/UserCard.dart
@@ -13,12 +13,13 @@ class UserCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Container(
       padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(5),
-        border: Border.all(color: selected ? Color.fromRGBO(236,151,0,1) : Color.fromRGBO(79,50,0,0.2),),
-        color: user.isAttended ? Color.fromRGBO(253,241,217,1) : Colors.white,
+        border: Border.all(color: Color.fromRGBO(79,50,0,0.2),),
+        color: user.isAttended || selected ? Color.fromRGBO(253,241,217,1) : Colors.white,
         
       ),
       child: FittedBox(
@@ -26,7 +27,6 @@ class UserCard extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
-              foregroundDecoration: BoxDecoration(color: selected ? Colors.grey.withOpacity(0.3) : null, shape: BoxShape.circle),
               child: CircleAvatar(
                 radius: MediaQuery.of(context).size.width / 8,
                 backgroundImage: user.imageLink.isNotEmpty ? Image.network(user.imageLink).image : null,
@@ -43,14 +43,14 @@ class UserCard extends StatelessWidget {
               padding: const EdgeInsets.symmetric(vertical: 4.0),
               child: Text(
                 user.userName,
-                style: TextStyle(color: selected ? Theme.of(context).primaryColor : Colors.black, fontSize: 18),
+                style: TextStyle(color: Colors.black, fontSize: 18),
               ),
             ),
             Row(
               children: [
                 Container(
                   padding: const EdgeInsets.all(1.0),
-                  child: Text("${user.groupName}", style: TextStyle(color: selected ? Theme.of(context).primaryColor : Colors.black, fontSize: 14, fontWeight: FontWeight.w300)),
+                  child: Text("${user.groupName}", style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.w300)),
                 ),
                 Container(
                   margin: EdgeInsets.only(right: 1),
