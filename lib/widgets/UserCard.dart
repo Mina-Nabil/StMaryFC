0f66cd1d5672dab60e6c3a1e5f8aec9212979912
Mina Nabil <mina9492@hotmail.com>
2020-11-17 20:54:27 +0200

    Adding Sized box only

--- lib/widgets/UserCard.dart
+++ lib/widgets/UserCard.dart
@@ -3,68 +3,77 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class UserCard extends StatelessWidget {
-  UserCard({
-    @required this.user,
-    @required this.selected
-  });
+  UserCard({@required this.user, @required this.selected});
 
   final User user;
   final bool selected;
 
   @override
   Widget build(BuildContext context) {
-
     return Container(
+      height: 80,
+      width: 30,
       padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(5),
-        border: Border.all(color: Color.fromRGBO(79,50,0,0.2),),
-        color: user.isAttended || selected ? Color.fromRGBO(253,241,217,1) : Colors.white,
-        
+        border: Border.all(
+          color: Color.fromRGBO(79, 50, 0, 0.2),
+        ),
+        color: user.isAttended || selected ? Color.fromRGBO(253, 241, 217, 1) : Colors.white,
       ),
-      child: FittedBox(
+
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Container(
-              child: CircleAvatar(
-                radius: MediaQuery.of(context).size.width / 8,
-                backgroundImage: user.imageLink.isNotEmpty ? Image.network(user.imageLink).image : null,
-                child: user.imageLink.isEmpty
-                    ? FittedBox(
-                        child: Text(
-                        _getInitials(),
-                        style: TextStyle(fontSize: 36, fontFamily: "Anton", color: Colors.white)
+            Flexible(
+              flex: 2,
+              child: Container(
+                child: CircleAvatar(
+                  radius: MediaQuery.of(context).size.width / 8,
+                  backgroundImage: user.imageLink.isNotEmpty ? Image.network(user.imageLink).image : null,
+                  child: user.imageLink.isEmpty
+                      ? FittedBox(child: Text(_getInitials(), style: TextStyle(fontSize: 36, fontFamily: "Anton", color: Colors.white)))
+                      : null,
+                ),
+              ),
+            ),
+            Flexible(
+                flex: 1,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: Text(
+                    user.userName,
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  ),
+                )),
+             Row(
+               mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(1.0),
+                    child: Text("${user.groupName}", style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.w300)),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(right: 1),
+                      child: FaIcon(
+                        FontAwesomeIcons.dollarSign,
+                        size: 12,
+                        color: user.isDue ? Colors.red : Colors.transparent,
+                      )),
+                  Container(
+                      margin: EdgeInsets.only(right: 1),
+                      child: FaIcon(
+                        FontAwesomeIcons.checkCircle,
+                        size: 12,
+                        color: user.isAttended ? Colors.green : Colors.transparent,
                       ))
-                    : null,
+                ],
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Text(
-                user.userName,
-                style: TextStyle(color: Colors.black, fontSize: 18),
-              ),
-            ),
-            Row(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(1.0),
-                  child: Text("${user.groupName}", style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.w300)),
-                ),
-                Container(
-                  margin: EdgeInsets.only(right: 1),
-                  child: FaIcon(FontAwesomeIcons.dollarSign, size: 12, color: user.isDue ? Colors.red : Colors.transparent,)
-                ),
-                Container(
-                  margin: EdgeInsets.only(right: 1),
-                  child: FaIcon(FontAwesomeIcons.checkCircle, size: 12, color: user.isAttended ? Colors.green : Colors.transparent,)
-                )
-              ],
-            ),
+     
           ],
         ),
-      ),
+  
     );
   }
 
