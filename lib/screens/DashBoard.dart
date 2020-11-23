3783bf707ffec0e93530432708149e6d6128e16f
Mina Nabil <mina9492@hotmail.com>
2020-11-24 01:34:29 +0200

    Adding bad network handling in web view

--- lib/screens/DashBoard.dart
+++ lib/screens/DashBoard.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:StMaryFA/screens/HomeScreen.dart';
 import 'package:connectivity/connectivity.dart';
 import 'package:flutter/cupertino.dart';
@@ -9,55 +11,92 @@ class DashBoard extends StatefulWidget {
   _DashBoardState createState() => _DashBoardState();
 }
 
-
-
 class _DashBoardState extends State<DashBoard> {
 
-bool isConnected = true;
-@override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration.zero).then((value) async {
-      var connectivityResult = await  (Connectivity().checkConnectivity());
-        isConnected = !(connectivityResult == ConnectivityResult.none);
-        if(connectivityResult == ConnectivityResult.none) {
-          showCupertinoDialog(
-          barrierDismissible: true,
-          context: context,
-          builder: (BuildContext context) => new CupertinoAlertDialog(
-                title: Text("Unable to connect"),
-                content: Text("Please check internet connection"),
-                actions: [
-                  CupertinoDialogAction(child: Text("Ok"), onPressed: () {
+  final int timeoutDuration = 15;
+
+  bool isConnected = true;
+  bool isLoading = true;
+  WebViewController controller;
+  Timer loadingTimer;
+
+  void showFailedConnectivity() {
+    setState(() {
+      isLoading = false;
+    });
+    showCupertinoDialog(
+        barrierDismissible: true,
+        context: context,
+        builder: (BuildContext context) => new CupertinoAlertDialog(
+              title: Text("Unable to connect"),
+              content: Text("Please check internet connection"),
+              actions: [
+                CupertinoDialogAction(
+                  child: Text("Ok"),
+                  onPressed: () {
                     Navigator.pushReplacement(
                       context,
                       MaterialPageRoute(builder: (context) => HomeScreen()),
                     );
-                  },)
-                ],
-              ));
-        }
+                  },
+                )
+              ],
+            ));
+  }
+
+  void startTimer() {}
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero).then((value) async {
+      var connectivityResult = await (Connectivity().checkConnectivity());
+      isConnected = !(connectivityResult == ConnectivityResult.none);
+      if (connectivityResult == ConnectivityResult.none) {
+        showFailedConnectivity();
+      }
     });
   }
 
   final _key = UniqueKey();
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-                preferredSize: Size(0, 0),
-                child: AppBar(
-                  elevation: 0,
-                  backgroundColor: Colors.transparent, // status bar color,
-                  brightness:  Brightness.light ,
-                )),
-      body: WebView(
-        key: _key,
-        initialUrl: 'https://stmaryfa.msquare.app/home',
-        javascriptMode: JavascriptMode.unrestricted,
+          preferredSize: Size(0, 0),
+          child: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.transparent, // status bar color,
+            brightness: Brightness.light,
+          )),
+      body: Stack(
+        children: [
+          WebView(
+            key: _key,
+            initialUrl: 'https://stmaryfa.msquare.app/home',
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (c) {
+              loadingTimer = new Timer(Duration(seconds: timeoutDuration), showFailedConnectivity);
+              isLoading = true;
+            },
+            onPageStarted: (string) {
+              setState(() {
+                if (!loadingTimer.isActive) {
+                  loadingTimer = new Timer(Duration(seconds: timeoutDuration), showFailedConnectivity);
+                }
+                isLoading = true;
+              });
+            },
+            onPageFinished: (string) {
+              setState(() {
+                isLoading = false;
+                loadingTimer.cancel();
+              });
+            },
+          ),
+          if (isLoading) Container(color: Colors.white, child: Center(child: CircularProgressIndicator()))
+        ],
       ),
     );
   }
