2ae67d4ad9f70bc2ac5323c1947aaf2eae947b23
michaelshabib <63905853+michaelshabib@users.noreply.github.com>
2021-01-05 01:36:25 +0200

    Enhance events picker

--- lib/widgets/NewEventPayment.dart
+++ lib/widgets/NewEventPayment.dart
@@ -24,7 +24,7 @@ class _NewPaymentState extends State<NewEventPayment> {
   final _eventController = TextEditingController();
   final _noteController = TextEditingController();
   
-  Event selectedEvent;
+  Event _event;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -94,52 +94,65 @@ class _NewPaymentState extends State<NewEventPayment> {
                               backgroundColor: Colors.transparent,
                               context: context, 
                               builder: (_) {
-                                return FutureBuilder(
-                                  future: Provider.of<EventsProvider>(context, listen: false).events == null? Provider.of<EventsProvider>(context, listen: false).loadEvents() : null,
-                                  builder: (context, snapshot) {
-                                    if(snapshot.connectionState == ConnectionState.waiting) {
-                                      return Container(
-                                        decoration: new BoxDecoration(
-                                        color: Colors.orangeAccent[100],
-                                        borderRadius: new BorderRadius.only(
-                                          topLeft: const Radius.circular(25.0),
-                                          topRight: const Radius.circular(25.0))
+                                Event selectedEvent;
+                                return Container(
+                                  decoration: new BoxDecoration(
+                                    color: Colors.orangeAccent[100],
+                                    borderRadius: new BorderRadius.only(
+                                      topLeft: const Radius.circular(15.0),
+                                      topRight: const Radius.circular(15.0))
+                                  ),
+                                  height: MediaQuery.of(context).size.height/3,
+                                  child: Column(
+                                    children:[
+                                      Align(
+                                        alignment: Alignment.centerRight,
+                                        child: FlatButton(
+                                          onPressed: (){
+                                            setState(() {
+                                              _eventController.value = TextEditingValue(text: selectedEvent.name);
+                                              _event = selectedEvent;
+                                               Navigator.pop(context);
+                                            });
+                                          }, 
+                                          child: Text("Done", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold ),),
                                         ),
-                                        height: MediaQuery.of(context).size.height/4,
-                                        child: Center(
-                                          child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.white),backgroundColor: Colors.orange,)),
-                                      );
-                                    } else {
-                                      List<Event> events = Provider.of<EventsProvider>(context, listen: false).events;
-                                      if(events.length == 1) {
-                                        selectedEvent = events[0];
-                                      }
-                                      return Container(
-                                        decoration: new BoxDecoration(
-                                        color: Colors.orangeAccent[100],
-                                        borderRadius: new BorderRadius.only(
-                                          topLeft: const Radius.circular(25.0),
-                                          topRight: const Radius.circular(25.0))
-                                        ),
-                                        height: MediaQuery.of(context).size.height/4,
-                                        child: CupertinoPicker(
-                                          itemExtent:  MediaQuery.of(context).size.height/16, 
-                                          onSelectedItemChanged: (i) {
-                                            selectedEvent = events[i];
-                                          },
-                                          
-                                          children: (events.map((event) {
-                                            return Center(child: Text(event.name));
-                                          }).toList()),
-                                        ),
-                                      );
-                                    }
-                                  },
+                                      ),
+                                      Expanded(
+                                        child: FutureBuilder(
+                                        future: Provider.of<EventsProvider>(context, listen: false).events == null? Provider.of<EventsProvider>(context, listen: false).loadEvents() : null,
+                                        builder: (context, snapshot) {
+                                          if(snapshot.connectionState == ConnectionState.waiting) {
+                                            return Container(
+                                              child: Center(
+                                                child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.white),backgroundColor: Colors.orange,)),
+                                            );
+                                          } else {
+                                            List<Event> events = Provider.of<EventsProvider>(context, listen: false).events;
+                                            if(events.isNotEmpty) {
+                                              selectedEvent = events[0];
+                                            }
+                                            return Container(
+                                              child: CupertinoPicker(
+                                                itemExtent:  MediaQuery.of(context).size.height/16, 
+                                                onSelectedItemChanged: (i) {
+                                                  selectedEvent = events[i];
+                                                },
+                                                
+                                                children: (events.map((event) {
+                                                  return Center(child: Text(event.name));
+                                                }).toList()),
+                                              ),
+                                            );
+                                          }
+                                        },
+                                    ),
+                                      ),
+                                  ]
+                                  ),
                                 );
                               }
-                            ).then((value) => setState(() {
-                              _eventController.value = TextEditingValue(text: selectedEvent.name);
-                            }));
+                            );
                             
                           }
                         ),
@@ -205,14 +218,14 @@ class _NewPaymentState extends State<NewEventPayment> {
         ));
     } else {
 
-      String errorMsg = await PaymentsHelper.addPayment(widget.id, double.parse(amount), note, 2, eventId: selectedEvent.id);
+      String errorMsg = await PaymentsHelper.addPayment(widget.id, double.parse(amount), note, 2, eventId: _event.id);
       if(errorMsg.isEmpty){
         _amountController.clear();
         _eventController.clear();
         _noteController.clear();
         setState(() {
           _open = !_open;
-          selectedEvent = null;
+          _event = null;
         });
         widget.onPaymentAdd();
       } else {
